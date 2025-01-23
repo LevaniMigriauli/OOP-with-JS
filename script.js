9c5240b52c741eb0c6675d3495c57c9440191693
Levani migriauli <levani.migriauli@lb.ge>
2025-01-23 21:35:25 +0400

    feat: constructor functions and new operator

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function(firstName, birthYear) {
+  // Instance properties (properties firstName and birthYear will be available on all the instances that are created through this constructor function)
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //  Never do this (each of constr.function object would carry around this function, terrible for performance. Prototypes and prototypal inheritance for solving this problem )
+  // this.calcAge = function() {
+  //   console.log(2037 - this.birthYear);
+  // }
+};
+
+const levan = new Person('Levan', 1997);
+console.log(levan);
+// When we call a function with the new operator, 4 steps behind the scenes
+// 1. New {} is created
+// 2. function is called, this = {} (this keyword will point to new empty object created in 1st step)
+// 3. this newly created object {} is linked to a prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(levan instanceof Person);
