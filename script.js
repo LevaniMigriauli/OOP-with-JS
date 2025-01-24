9570ae87fae642a455e07e212c422c353aa0b43e
Levani migriauli <levani.migriauli@lb.ge>
2025-01-24 22:58:30 +0400

    feat: ES6 Classes

--- script.js
+++ script.js
@@ -69,3 +69,38 @@ console.log(arr.unique())
 const h1 = document.querySelector('h1')
 console.dir(h1)
 console.dir(x => x + 1)
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor (firstName, birthYear) {
+    this.firstName = firstName
+    this.birthYear = birthYear
+  }
+
+  // Methods will be added to .prototype property
+  calcAge () {
+    console.log(2037 - this.birthYear)
+  }
+
+  greet () {
+    console.log(`Hey ${this.firstName}`)
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996)
+console.log(jessica)
+jessica.calcAge()
+
+console.log(jessica.__proto__ === PersonCl.prototype)
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`)
+// }
+jessica.greet()
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
