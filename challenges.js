440d0e6d311dbc7cf4ae7d519fe15c622c9c219e
Levani migriauli <levani.migriauli@lb.ge>
2025-01-26 20:29:00 +0400

    feat: accomplish 4th challenge

--- challenges.js
+++ challenges.js
@@ -127,3 +127,68 @@ tesla.chargeBattery(50)
 tesla.accelerate()
 tesla.brake()
 */
+
+///////////////////////////////////////
+// Coding Challenge #4
+// 1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+// 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+// 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+class CarCl {
+  constructor (make, speed) {
+    this.make = make
+    this.speed = speed
+  }
+
+  accelerate () {
+    this.speed += 10
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+  }
+
+  brake () {
+    this.speed -= 5
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+    return this
+  }
+
+  get speedUS () {
+    return this.speed / 1.6
+  }
+
+  set speedUS (speed) {
+    this.speed = speed * 1.6
+  }
+}
+
+class EVCl extends CarCl {
+  #charge
+
+  constructor (make, speed, charge) {
+    super(make, speed)
+    this.#charge = charge
+  }
+
+  chargeBattery = function (chargeTo) {
+    this.#charge = chargeTo
+    return this
+  }
+  accelerate = function () {
+    this.speed += 20
+    this.#charge--
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${this.#charge} %`)
+    return this
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23)
+console.log(rivian)
+rivian.accelerate().
+  accelerate().
+  accelerate().
+  brake().
+  chargeBattery(50).
+  accelerate()
+
+console.log(rivian.speedUS)
